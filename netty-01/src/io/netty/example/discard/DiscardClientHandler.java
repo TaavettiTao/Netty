3dba9ff8792c77fd896409dedcf33e5317dca4a8
TaavettiTao <1634093385@qq.com>
2017-02-21 09:55:25 +0800

    update

--- netty-01/src/io/netty/example/discard/DiscardClientHandler.java
+++ netty-01/src/io/netty/example/discard/DiscardClientHandler.java
@@ -7,25 +7,35 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class DiscardClientHandler extends ChannelInboundHandlerAdapter {
-    @Override
+    
+    //接收服务端消息
+	@Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		ByteBuf buffer=(ByteBuf)msg;
+		int len=buffer.readableBytes();
+		byte[] bytes=new byte[len];
+		buffer.readBytes(bytes);
+		System.out.println("服务端响应数据："+new String(bytes));
     	
     }
     
     
 
+    //连接成功，向服务端发送消息
     @Override
 	public void channelActive(final ChannelHandlerContext ctx) throws Exception {
     	final ByteBuf time = ctx.alloc().buffer(4); // (2)
         time.writeBytes("sssss".getBytes());
         final ChannelFuture f = ctx.writeAndFlush(time); // (3)
-        f.addListener(new ChannelFutureListener() {
+        
+       /* 如果加上以下代码，客户端在连接服务器端发送信息之后，即会把连接断掉，此时将无法接收服务端响应，若希望接收响应，则不添加此代码，连接的关闭交由服务端做即可。
+        * f.addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture future) {
                 assert f == future;
                 ctx.close();
             }
-        });
+        });*/
 	}
 
 
--- netty-01/src/io/netty/example/discard/DiscardServerHandler.java
+++ netty-01/src/io/netty/example/discard/DiscardServerHandler.java
@@ -1,6 +1,8 @@
 package io.netty.example.discard;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -10,7 +12,7 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+    public void channelRead(final ChannelHandlerContext ctx, Object msg) { // (2)
     	//读取客户端数据
     	ByteBuf buf=(ByteBuf)msg;
         int bytes=buf.readableBytes();
@@ -18,6 +20,18 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
         byte[] data=new byte[bytes];
         buf.readBytes(data);
         System.out.println("读取数据："+new String(data));
+        
+        //服务端在接收到客户端数据处理之后，给客户端返回响应，之后关闭通道连接。
+        final ByteBuf time = ctx.alloc().buffer(4); // (2)
+        time.writeBytes("server receives data!".getBytes());
+        final ChannelFuture f = ctx.writeAndFlush(time); // (3)
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                assert f == future;
+                ctx.close();
+            }
+        });
     }
 
     @Override
